cb4f87502cb77320554d13184cf0add7375e907f
sfoxover <sfoxover@gmail.com>
2019-10-08 22:41:08 -0700

    - inital checkin

---
+++ RPiCameraClient/ImportSubscriber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace RPiCameraClient
+{
+    public class ImportSubscriber
+    {
+        [DllImport("RPiClientLib.dll", EntryPoint = "#2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool StartSubscription();
+
+    }
+}
