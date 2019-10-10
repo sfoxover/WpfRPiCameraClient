141b5c6bd5a19bf9cd616fa1de977b1d96ab15cf
sfoxover <sfoxover@gmail.com>
2019-10-10 15:09:23 -0700

    - check in progress

--- RPiCameraClient/MainWindow.xaml.cs
+++ RPiCameraClient/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace RPiCameraClient
 {
@@ -35,14 +36,36 @@ namespace RPiCameraClient
                 do
                 {
                     int size = 0;
-                    ImportSubscriber.GetCurrentImage(out byte[] buffer, ref size);
+                    IntPtr pBuffer = IntPtr.Zero;
+                    ImportSubscriber.GetCurrentImage(ref pBuffer, ref size);
                     if (size > 0)
                     {
-                        Image image = Image.FromStream(new MemoryStream(buffer));
+                        byte[] imgBuffer = new byte[size];
+                        Marshal.Copy(pBuffer, imgBuffer, 0, imgBuffer.Length);
+
+                        Application.Current.Dispatcher.Invoke(new Action(() => 
+                        { 
+                            VideoImg.Source = LoadImage(new MemoryStream(imgBuffer)); 
+                        }));                        
                     }
                     // int size = imageBuffer.Length;
                 } while (WaitEvent.WaitOne(100) == false);
             });
+        }
+
+        private BitmapImage LoadImage(Stream stream)
+        {
+            // assumes that the streams position is at the beginning
+            // for example if you use a memory stream you might need to point it to 0 first
+            var image = new BitmapImage();
+
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.StreamSource = stream;
+            image.EndInit();
+
+            image.Freeze();
+            return image;
         }
     }
 }
