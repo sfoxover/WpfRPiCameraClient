7de9c2908cadeed9b90ca55a0748629e34bf37c7
sfoxover <sfoxover@gmail.com>
2019-10-21 16:04:31 -0700

    - fix wrapper imports

--- RPiCameraClient/ImportSubscriber.cs
+++ RPiCameraClient/ImportSubscriber.cs
@@ -7,13 +7,13 @@ namespace RPiCameraClient
 {
     public class ImportSubscriber
     {
-        [DllImport("RPiClientLib.dll", EntryPoint = "#2", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        [DllImport("RPiClientWrapperLib.dll", EntryPoint = "#2", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
         public static extern bool StartSubscription(string topic);
 
-        [DllImport("RPiClientLib.dll", EntryPoint = "#3", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("RPiClientWrapperLib.dll", EntryPoint = "#3", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool StopSubscription();
 
-        [DllImport("RPiClientLib.dll", EntryPoint = "#1", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("RPiClientWrapperLib.dll", EntryPoint = "#1", CallingConvention = CallingConvention.Cdecl)]
         public static extern void GetCurrentImage(ref IntPtr buffer, ref int size);
 
     }
