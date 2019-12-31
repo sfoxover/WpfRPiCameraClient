d2740371b5b3b2f9b8b0e2c44214b36b9c21f6d9
sfoxover <sfoxover@gmail.com>
2019-12-30 20:33:33 -0800

    - use new zeromq library

--- SocketLibrary/pch.cpp
+++
@@ -1,7 +0,0 @@
-// pch.cpp: source file corresponding to the pre-compiled header
-
-#include "pch.h"
-
-// Static libs
-#pragma comment(lib, "MessagesStaticLib.lib")
-#pragma comment(lib, "JsoncppStaticLib.lib")
