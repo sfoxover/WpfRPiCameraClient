4d7bedc8b52126f6c43114973c3c44be95974f84
sfoxover <sfoxover@gmail.com>
2020-01-01 15:45:34 -0800

    - test new mat encoding

---
+++ MessagesLibrary/MessageFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessagesLibrary
+{
+    public class MessageFactory
+    {
+		// Create message from key value pairs
+		public static Message Create(string topic, Int32 type, Dictionary<string, object> items)
+		{
+			var msg = new Message();
+
+			// Set topic and type
+			msg.SetTopic(topic);
+			msg.SetMessageType(type);
+			msg.SetMicroTime();
+			msg.HeaderMap.AddDictionary(items);
+			return msg;
+		}
+
+		// Create message from data buffer
+		public static Message Create(string topic, Int32 type, byte[] buffer)
+		{
+			Message msg = new Message();
+
+			// Set topic and type
+			msg.SetTopic(topic);
+			msg.SetMessageType(type);
+			msg.SetMicroTime();
+
+			// Copy to buffer
+			msg.SetData(buffer);
+			return msg;
+		}
+	}
+}
